883a61487cc8e9ac55a273100c939088f79c4070
Luke Wheldale <lukewheldale@gmail.com>
2024-02-23 18:51:06 +1030

    factory function for Player and IIFE module for gameboard

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+// IIFE Module for gameboard
+const Gameboard = (function () {
+	const gameboard = [
+		["x", "x", "x"],
+		["x", "x", "x"],
+		["x", "x", "x"],
+	];
+	return gameboard;
+})();
+console.log(Gameboard);
 
+// Factorie function (Create player objects)
+function Player(playerName) {
+	return {
+		playerName: playerName,
+	};
+}
+
+let playerOne = Player("player one");
+let playerTwo = Player("player Two");
+
+console.log(playerOne, playerTwo);
+
+// Control flow of game
+const Game = {};
+
+// Module
+// Display Controller
