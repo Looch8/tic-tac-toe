5a62bcc0fa9ed5c5fd15c5001d8aea7bbb424735
Luke Wheldale <lukewheldale@gmail.com>
2024-02-23 21:31:49 +1030

    dynamic gameboard function

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 // IIFE Module for gameboard
 const Gameboard = (function () {
-	const gameboard = [
-		["x", "x", "x"],
-		["x", "x", "x"],
-		["x", "x", "x"],
-	];
-	return gameboard;
+	const rows = 3;
+	const columns = 3;
+	const board = [];
+
+	// Create 2-d array
+	for (let i = 0; i < rows; i++) {
+		board[i] = [];
+		for (let j = 0; j < columns; j++) {
+			board[i].push(Cell());
+		}
+	}
+	return board;
 })();
+
+// Cell represents one square on the board
+function Cell() {
+	let value = 0;
+	return {
+		value,
+	};
+}
+
 console.log(Gameboard);
 
 // Factorie function (Create player objects)
