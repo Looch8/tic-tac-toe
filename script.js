cffcef9e61a5964f9b4222291d4adf214fb7602b
Luke Wheldale <lukewheldale@gmail.com>
2024-02-26 22:04:50 +1030

    revert back to old changes

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
 // IIFE Module for gameboard
-const gameboard = (function () {
-	const rows = 3;
-	const columns = 3;
-	const board = ["", "", "", "", "", "", "", "", ""];
-
-	// render the board when start is clicked
-
-	const render = () => {
-		let boardHTML = "";
-		gameboard.forEach((square, index) => {
-			boardHTML += `<div class="square" id=square-${index}">${square}</div>`;
-		});
-	};
-	document.querySelector(".gameboard").innerHTML = boardHTML;
-	return {
-		render,
-	};
+const Gameboard = (function () {
+	const gameboard = [
+		["x", "x", "x"],
+		["x", "x", "x"],
+		["x", "x", "x"],
+	];
+	return gameboard;
 })();
+console.log(Gameboard);
 
 // Factorie function (Create player objects)
 function createPlayer(player) {
@@ -31,28 +22,7 @@ let playerTwo = createPlayer("player Two");
 console.log(playerOne, playerTwo);
 
 // Control flow of game
-const Game = () => {
-
-	let players = [];
-	let currentPlayerIndex
-	let gameOver
-
-	const start = () => {
-		players = [createPlayer(document.querySelector("#player1").value, 'O'), createPlayer(document.querySelector("#player2").value, 'X')]
-		currentPlayerIndex = 0;
-		gameOver = false;
-		gameboard.render();
-	}
-	return {
-		start,
-	}
-}();
+const Game = {};
 
 // Module
 // Display Controller
-
-// Start game
-const startButton = document.querySelector("#start-button");
-startButton.addEventListener("click", () => {
-	//Game.start();
-});
